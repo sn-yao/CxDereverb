4be39d237af2f92e1f3356b4f42f362db6e84cc0
sn-yao <snyao@gm.ntpu.edu.tw>
2025-08-12 11:36:42 +0800

    Update calSD.m

--- CxDereverb_basic/calSD.m
+++ CxDereverb_basic/calSD.m
@@ -1,3 +1,17 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% <INPUTs>
+% x        : room model.
+% fs       : sampling rate.
+% <OUTPUTs>
+% sd       : spectrum distortion from 0 Hz to fs/2 Hz.
+% sdx      : spectrum distortion from 100 Hz to 16000 Hz.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% <EXAMPLE>
+% x = load('small.mat');
+% fs = 44100;
+% plot(linspace(0,50,1e4), MUSIC);
+% disp(sdx);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [sd,sdx] = calSD(x,fs)
 x=x./max(abs(x));
 if (nargin < 2)
@@ -7,4 +21,5 @@ fx = fft(x,fs);
 freq = (0 : fs/2);
 sd = std(20*log10(abs(fx(1:(fs/2 + 1)))));
 sdx=std(20*log10(abs(fx(100:16000))));
+
 
